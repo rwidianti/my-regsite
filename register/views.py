83b5f6fc5cf9167eb77be27e82e8282460ed6ca4
rwidianti <r.widianti@yahoo.com>
2017-07-30 18:17:12 +0700

    tambah model

--- register/views.py
+++ register/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import regdata
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'register/regdata_list.html', {})
+        regdatas = regdata.objects.all()
+        return render(request, 'register/regdata_list.html', {'regdatas': regdatas})
